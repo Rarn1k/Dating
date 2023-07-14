7fbcf13122f77be559df810e1d7b22ac209f86f9
unknown <alyosha.plotnikov2001@mail.ru>
2023-07-15 02:11:23 +0400

    added watermark

--- auth/serializers/register.py
+++ auth/serializers/register.py
@@ -1,5 +1,12 @@
+import io
+import os
+import sys
+
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import serializers
 
+from Dating import settings
 from user.models import User
 from user.serializers import UserSerializer
 
@@ -12,4 +19,15 @@ class RegisterSerializer(UserSerializer):
         fields = ['email', 'password', 'first_name', 'last_name', 'gender', 'avatar', ]
 
     def create(self, validated_data):
+        try:
+            base_avatar = Image.open(validated_data["avatar"])
+            watermark = Image.open(os.path.join(os.path.dirname(settings.MEDIA_ROOT), 'media', 'water_mark.png'))
+            base_avatar.paste(watermark, (0, 0), mask=watermark)
+            output = io.BytesIO()
+            base_avatar.save(output, format='JPEG')
+            output.seek(0)
+            validated_data["avatar"] = InMemoryUploadedFile(output, 'ImageField', 'avatar.jpg', 'image/jpeg',
+                                                            sys.getsizeof(output), None)
+        except KeyError:
+            pass
         return User.objects.create_user(**validated_data)
